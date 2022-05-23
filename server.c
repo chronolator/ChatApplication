221471938546e27d042d6d67380e52736af2b8fa
chronolator <mikestuzi@gmail.com>
2022-05-23 11:07:10 -0400

    Added comments bc why not lol

--- server.c
+++ server.c
@@ -22,32 +22,36 @@ int main(int argc, char *argv[]){
     struct sockaddr_in server_addr, client_addr;
     socklen_t client_len;
 
-    // Create a TCP socket
+    // Create a socket of type IPv4, with two-way connection, and protocol TCP
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if(sockfd < 0) {
         error("Error opening socket");
     }
 
-    // Fills memory with a constant byte
+    // Zero out struct, convert argv[1] to an int, set the server typei to IPv4, listening range, and port
     memset((char *) &server_addr, 0, sizeof(server_addr));
     portno = atoi(argv[1]);
-
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(portno);
 
+    // Bind to the socket
     if(bind(sockfd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
         error("Binding Failed");
     }
 
+    // Listen on socket with a backlog(concurrent clients) of 5
     listen(sockfd, 5);
     client_len = sizeof(client_addr);
 
+    // Accept the socket from the client
     newsockfd = accept(sockfd, (struct sockaddr*) &client_addr, &client_len);
     if(newsockfd < 0) {
         error("Error on accept");
     }
 
+    // Zero out the buffer, read data from the newsocketfd into the buffer, print out the data for the server, 
+    // Zero out the buffer, get stdin, write the data to the socket for the client, 
     for(;;) {
         memset((char *) buf, 0, sizeof(buf));
         n = read(newsockfd, buf, sizeof(256));
@@ -64,6 +68,7 @@ int main(int argc, char *argv[]){
             error("Error on writing");
         }
 
+        // If the server message is "Bye", break out of the loop
         int i = strncmp("Bye", buf, 3);
         if(i == 0) { break; }
 
