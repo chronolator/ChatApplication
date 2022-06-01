e6a83fbf7fbc4af21ea1dccb58fbaf81ad427dbb
chronolator <mikestuzi@gmail.com>
2022-06-01 18:50:30 -0400

    modified:   client.c 	modified:   server.c

--- client.c
+++ client.c
@@ -5,67 +5,75 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <netdb.h>
+#include <pthread.h>
+#include <signal.h>
+
+#define MSG_LEN 2048
 
 // Function prototypes
 void print_usage();
 void error(const char *msg);
+void sig_handler();
+void str_rm_nl(char *arr, int len);
+void recv_handler();
+void send_handler();
 
 int main(int argc, char *argv[]) {
 
-    // Establish variables
-    int sockfd, portno, n;
-    struct sockaddr_in server_addr;
-    struct hostent *server;
-    char buf[256];
+    // Catch sigint
+    signal(SIGINT, sig_handler);
 
-    if(argc < 3) {
-        fprintf(stderr, "usage %s, hostname port\n", argv[0]);
+    // Create socket
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(sockfd == -1) {
+        printf("Faield to create a socket\n");
         exit(1);
     }
 
-    // Get port and create socket
-    portno = atoi(argv[2]);
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if(sockfd < 0) {
-        error("Error opening socket");
-    }
-    
-    // Set the server IP
-    server = gethostbyname(argv[1]);
-    if(server == NULL) {
-        fprintf(stderr, "Error no such IP/host");
+    // Socket settings
+    struct sockaddr_in server, client;
+    int s_len = sizeof(server);
+    int c_len = sizeof(client);
+    memset(&server, 0, sizeof(s_len));
+    memset(&client, 0, sizeof(c_len));
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_port = htons(1337);
+
+    // Connect to socket
+    int err = connect(sockfd, (struct sockaddr *) &server, s_len);
+    if(err == -1) {
+        printf("Failed to connect to socket\n");
+        exit(1);
     }
 
-    // Zero out server_addr, set to IPv4, copy data from server->h_addr to server_addr.sin_addr.sin_addr
-    memset((char *) &server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    memcpy((char *) &server_addr.sin_addr.s_addr, (char *) server->h_addr, server->h_length);
-    server_addr.sin_port = htons(portno);
-    if(connect(sockfd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
-        error("Connection failed");
+    // Header info
+    getsockname(sockfd, (struct sockaddr *) &client, (socklen_t *) &c_len);
+    getpeername(sockfd, (struct sockaddr *) &server, (socklen_t *) &s_len);
+    printf("Connected to server: %s:%d\n", inet_ntoa(server.sin_addr), ntohs(server.sin_port));
+    printf("You are: %s:%d\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
+
+    // Thread sending messages
+    pthread_t send_thread;
+    if(pthread_create(&send_thread, NULL, (void *) send_handler, NULL) != 0) {
+        printf("Failed to create pthread\n");
+        exit(1);
     }
 
+    // Thread receiving messages
+    pthread_t recv_thread;
+    if(pthread_create(&recv_thread, NULL, (void *) recv_handler, NULL) != 0) {
+        printf("Failed to create pthread\n");
+        exit(1);
+    }
+
+    // Keep socket open
     for(;;) {
-        memset((char *) buf, 0, sizeof(buf));
-        fgets(buf, sizeof(buf), stdin);
-        n = write(sockfd, buf, strlen(buf));
-        if(n < 0) {
-            error("Error writing");
-        }
-
-        memset((char *) buf, 0, sizeof(buf));
-        n = read(sockfd, buf, sizeof(buf));
-        if(n < 0) { 
-            error("Error reading"); 
-        }
-
-        printf("Server: %s\n", buf);
-        int i = strncmp("Bye", buf, 3);
-        if(i == 0) { break; }
     }
-    close(sockfd);
 
+    close(sockfd);
     return EXIT_SUCCESS;
 }
 
@@ -77,4 +85,46 @@ void print_usage(char *in) {
 void error(const char *msg) {
     perror(msg);
     exit(1);
+}
+
+void sig_handler(int sockfd) {
+    close(sockfd);
+    exit(1);
+}
+
+void str_rm_nl(char *arr, int len) {
+    int i;
+    for(i = 0; i < len; i++) {
+        if(arr[i] == '\n') {
+            arr[i] = '\0';
+            break;
+        }
+    }
+}
+
+void recv_handler(int sockfd) {
+    char rmsg[MSG_LEN] = {};
+    for(;;) {
+        int r = recv(sockfd, rmsg, MSG_LEN, 0);
+        if(r > 0) {
+            printf("%s\n", rmsg);
+        }
+        else {
+            break;
+        }
+    }
+}
+
+void send_handler(int sockfd) {
+    char smsg[MSG_LEN] = {};
+    printf("> ");
+    for(;;) {
+        while(fgets(smsg, sizeof(smsg), stdin) != NULL) {
+            str_rm_nl(smsg, MSG_LEN);
+        }
+        send(sockfd, smsg, MSG_LEN, 0);
+        if(strcmp(smsg, "exit") == 0) {
+            break;
+        }
+    }
 }
--- server.c
+++ server.c
@@ -5,8 +5,17 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
 
+#define MSG_LEN 2048
+#define NAME_LEN 32
+
+void print_usage();
 void error(const char *msg);
+void str_rm_nl(char *arr, int len);
+void recv_handler();
+void send_handler();
 
 int main(int argc, char *argv[]){
 
@@ -17,8 +26,8 @@ int main(int argc, char *argv[]){
     }
 
     // Establish variables
-    int sockfd, newsockfd, portno, n;
-    char buf[256];
+    int sockfd, newsockfd, portno;
+    const int bl = 5;
     struct sockaddr_in server_addr, client_addr;
     socklen_t client_len;
 
@@ -41,45 +50,88 @@ int main(int argc, char *argv[]){
     }
 
     // Listen on socket with a backlog(concurrent clients) of 5
-    listen(sockfd, 5);
+    listen(sockfd, bl);
     client_len = sizeof(client_addr);
 
-    // Accept the socket from the client
-    newsockfd = accept(sockfd, (struct sockaddr*) &client_addr, &client_len);
-    if(newsockfd < 0) {
-        error("Error on accept");
-    }
+    // Server header
+    getsockname(sockfd, (struct sockaddr*) &server_addr, (socklen_t *) &client_len);
+    printf("Server IP: %s:%d\n", inet_ntoa(server_addr.sin_addr), ntohs(server_addr.sin_port));
+    printf("Backlog: %d\n", bl);
 
-    // Zero out the buffer, read data from the newsocketfd into the buffer, print out the data for the server, 
-    // Zero out the buffer, get stdin, write the data to the socket for the client, 
     for(;;) {
-        memset((char *) buf, 0, sizeof(buf));
-        n = read(newsockfd, buf, sizeof(256));
-        if(n < 0) {
-            error("Error on reading");
+        // Accept the socket from the client
+        newsockfd = accept(sockfd, (struct sockaddr*) &client_addr, &client_len);
+        if(newsockfd < 0) {
+            error("Error on accept");
         }
 
-        printf("Client: %s\n", buf);
-        memset((char *) buf, 0, sizeof(buf));
-        fgets(buf, sizeof(buf), stdin);
+        // Print Client IP
+        getpeername(newsockfd, (struct sockaddr*) &client_addr, (socklen_t*) &client_len);
+        printf("Client from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
 
-        n = write(newsockfd, buf, strlen(buf));
-        if(n < 0) {
-            error("Error on writing");
+        // Thread sending messages
+        //pthread_t send_thread;
+        //if(pthread_create(&send_thread, NULL, (void *) send_handler, NULL) != 0) {
+        //    printf("Failed to create send pthread\n");
+        //    exit(1);
+        //}
+
+        // Thread receiving messages
+        pthread_t recv_thread;
+        if(pthread_create(&recv_thread, NULL, (void *) recv_handler, NULL) != 0) {
+            printf("Failed to create recv pthread\n");
+            exit(1);
         }
-
-        // If the server message is "Bye", break out of the loop
-        int i = strncmp("Bye", buf, 3);
-        if(i == 0) { break; }
 
     }
-    close(newsockfd);
+
     close(sockfd);
 
     return EXIT_SUCCESS;
 }
 
+void print_usage(char *in) {
+    printf("Usage: %s <PORT>\n", in);
+    printf("Usage: Will require 'sudo' to run if the executable is not setuid root\n");
+}
+
 void error(const char *msg) {
     perror(msg);
     exit(1);
+}
+
+void str_rm_nl(char *arr, int len) {
+    int i;
+    for(i = 0; i < len; i++) {
+        if(arr[i] == '\n') {
+            arr[i] = '\0';
+            break;
+        }
+    }
+}
+
+void recv_handler(int sockfd) {
+    char rmsg[MSG_LEN] = {};
+    for(;;) {
+        int r = recv(sockfd, rmsg, MSG_LEN, 0);
+        if(r > 0) {
+            printf("%s\n", rmsg);   
+        } else {
+            break;
+        }
+    }
+}
+
+void send_handler(int sockfd) {
+    char smsg[MSG_LEN] = {};
+    for(;;) {
+        printf("> ");
+        while(fgets(smsg, sizeof(smsg), stdin) != NULL) {
+            str_rm_nl(smsg, MSG_LEN);
+        }
+        send(sockfd, smsg, MSG_LEN, 0);
+        if(strcmp(smsg, "exit") == 0) {
+            break;
+        }
+    }
 }
