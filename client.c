74a55b31d61d5abd3093bcb8cf6c1341d64cab3c
chronolator <mikestuzi@gmail.com>
2022-07-05 19:58:48 -0500

    modified:   bin/server 	modified:   client.c 	modified:   server.c

--- client.c
+++ client.c
@@ -1,63 +1,106 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <pthread.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netdb.h>
+#include <signal.h>
 
-// Function prototypes
-void print_usage();
+#define BUF_SIZE 256
+
+void *recMsg(void *socket);
+void sig_handler(int signo);
 
 int main(int argc, char *argv[]) {
+    const int PORT = 8000;
 
-    // Get exactly two commandline arguments
-    if(argc < 3 || argc > 3) {
-        print_usage(argv[0]);
+    int ret;
+    int sockfd = (int) socket;
+    char buffer[BUF_SIZE];
+    char *server_address;
+    pthread_t rThread;
+    struct sockaddr_in ser_addr, cli_addr;
+        ser_addr.sin_family = AF_INET;
+        ser_addr.sin_port = htons(PORT);
+        ser_addr.sin_addr.s_addr = INADDR_ANY;
+
+    // Args
+    if(argc < 2) {
+        printf("usage: client < ip address >\n");
+        exit(1);
+    }
+    server_address = argv[1];
+
+    // Catch sigint
+    if(signal(SIGINT, sig_handler) == SIG_ERR) {
+        printf("Unable to catch SIGINT\n");
         exit(1);
     }
 
-    // Start of socket
-    // TODO: Make IP not a string and make IP a const
-    const char *IP = argv[1];
-    const int PORT = atoi(argv[2]);
+    // Create socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(sockfd < 0) {
+        printf("Error creating socket\n");
+        exit(1);
+    }
+    printf("Socket created\n");
 
-    if(PORT == 0) {
-        printf("Port value must be a numerical value\n\n");
-        print_usage(argv[0]);
+    // Conenct to socket
+    ret = connect(sockfd, (struct sockaddr_in *) &ser_addr, sizeof(ser_addr));
+    if(ret < 0) {
+        printf("Error connecting to the server\n");
+        exit(1);
+    }
+    printf("Connected to the server\n");
+
+    // Create pthread for receiving messages from server
+    printf("CLIENT> ");
+    ret = pthread_create(&rThread, NULL, recMsg, (void *) sockfd);
+    if(ret < 0) {
+        printf("Error from pthread_create() is %d\n", ret);
         exit(1);
     }
 
-    printf("IP: %s \n", IP);
-    printf("PORT: %d\n", PORT);
-
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    struct sockaddr_in ssock;
-        ssock.sin_family = AF_INET;
-        ssock.sin_port = htons(PORT);
-        ssock.sin_addr.s_addr = INADDR_ANY;
-
-
-    for(;;) {
-        int sock_state = connect(sockfd, (struct sockaddr *) &ssock, sizeof(ssock));
-        if(sock_state != 0 ) {
-            printf("There was an error with making a connection to the server\n");
-            exit(1);
+    // Stdin data sent to the server
+    while(fgets(buffer, BUF_SIZE, stdin) != NULL) {
+        ret = sendto(sockfd, buffer, BUF_SIZE, 0, (struct sockaddr *) &ser_addr, sizeof(ser_addr));
+        if(ret < 0) {
+            printf("Error sending the data below:\n\t%s\n", buffer);
         }
-
-        char ssend[1024] = "";
-        recv(sockfd, &ssend, sizeof(ssend), 0);
-        printf("Server Response: %s\n", ssend);
-        
-        close(sockfd);
     }
-
-
-
+    
+    // Close and return everything
+    close(sockfd);
+    pthread_exit(NULL);
     return EXIT_SUCCESS;
 }
 
-// Function for print usage
-void print_usage(char *in) {
-    printf("Usage: %s <IP> <PORT>\n", in);
-    printf("Usage: Will require 'sudo' to run if the executable is not setuid root\n");
+void *recMsg(void *socket) {
+    int ret;
+    int sockfd = (int) socket;
+    char buffer[BUF_SIZE];
+
+    for(;;) {
+        ret = recvfrom(sockfd, buffer, BUF_SIZE, 0, NULL, NULL);
+
+        if(ret < 0) {
+            printf("Error receiving data\n");   
+        } else {
+            printf("server: %s\n", buffer);
+            //printf("server: "),;
+            //fputs(buffer, stdout);
+            //printf("\n");
+        }
+    }
+}
+
+void sig_handler(int signo) {
+    if(signo == SIGINT) {
+
+        printf("Caught sigint\n");
+        exit(1);
+    }
 }
--- server.c
+++ server.c
@@ -1,56 +1,127 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
+#include <string.h>
+#include <pthread.h>
+#include <signal.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <netinet/in.h>
 
-void print_usage(char *in);
+#define BUF_SIZE 256
+#define CLIADDR_LEN 100
 
-int main(int argc, char *argv[]) {
+int gsockfd, gnewsockfd;
 
-    if(argc < 3 || argc > 3) {
-        print_usage(argv[0]);
+void *recMsg(void *sock);
+void sig_handler(int signo, int server, int client);
+
+int main(int argc, char *argv[]){ 
+    const int PORT = 8000;
+
+    int sockfd, len, ret, newsockfd;
+    char buffer[BUF_SIZE];
+    char clientAddr[CLIADDR_LEN];
+    pthread_t rThread;
+    pid_t childpid;
+    struct sockaddr_in ser_addr, cli_addr;
+        ser_addr.sin_family = AF_INET;
+        ser_addr.sin_port = htons(PORT);
+        ser_addr.sin_addr.s_addr = INADDR_ANY;
+        
+
+    // Catch sigint
+    if(signal(SIGINT, sig_handler) == SIG_ERR) {
+        printf("Unable to catch SIGINT\n");
         exit(1);
     }
 
-    const char *IP = argv[1];
-    const int PORT = atoi(argv[2]);
-    const char smsg[64] = "This is a message from the server LOL";
+    // Create socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    gsockfd = sockfd;
+    if(sockfd < 0) {
+        printf("Error creating socket\n");
+        exit(1);
+    }
+    printf("Socket created\n");
 
-    if(PORT == 0) {
-        printf("Port value must be a numerical value\n\n");
-        print_usage(argv[0]);
+    // Bind to socket
+    ret = bind(sockfd, (struct sockaddr *) &ser_addr, sizeof(ser_addr));
+    if(ret < 0) {
+        printf("Error binding to socket\n");
+        exit(1);
+    }
+    printf("Bound to socket\n");
+
+    // Socket is listening
+    printf("\nListenting for any incoming clients\n");
+    listen(sockfd, 5);
+
+    // Accept incoming connections
+    len = sizeof(cli_addr);
+    printf("Size of cli_addr: %d\n", cli_addr);
+    printf("Size of ser_addr: %d\n", ser_addr);
+    newsockfd = accept(sockfd, (struct sockaddr*) &cli_addr, &len);
+    gnewsockfd = newsockfd;
+    if(newsockfd < 0) {
+        printf("Error accepting connection\n");
         exit(1);
     }
 
-    printf("IP: %s\n", IP);
-    printf("PORT: %d\n", PORT);
+    // Convet IPv4 and IPV6 address from binary to text form
+    inet_ntop(AF_INET, &(cli_addr.sin_addr), clientAddr, CLIADDR_LEN);
+    printf("Connection accepted from %s...\n", clientAddr);
+    
+    // Create a pthread for receiving messages from the client
+    printf("SERVER> ");
+    ret = pthread_create(&rThread, NULL, recMsg, (void *) newsockfd);
+    if(ret < 0) {
+        printf("Error from pthread_create() is %d\n", ret);
+        exit(1);
+    }
 
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    struct sockaddr_in ssock;
-        ssock.sin_family = AF_INET;
-        ssock.sin_port = htons(PORT);
-        ssock.sin_addr.s_addr = INADDR_ANY;
-
-    bind(sockfd, (struct sockaddr*) &ssock, sizeof(ssock));
-
-    for(;;) {
-        listen(sockfd, 5);
-        int csock = accept(sockfd, NULL, NULL);
-        if(csock != -1) {
-            printf("Client has connected with a return value of %d\n", csock);
+    // Stdin data sent to the client
+    while(fgets(buffer, BUF_SIZE, stdin) != NULL) {
+        ret = sendto(newsockfd, buffer, BUF_SIZE, 0, (struct sockaddr*) &cli_addr, len);
+        if(ret < 0) {
+            printf("Error sending the data below to the client:\n\t%s\n", buffer);
+            exit(1);
         }
-        send(csock, smsg, sizeof(smsg), 0);
-        close(sockfd);
     }
 
+    // Close and return everything 
+    close(newsockfd);
+    close(sockfd);
+    pthread_exit(NULL);
     return EXIT_SUCCESS;
 }
 
-void print_usage(char *in) {
-    printf("Usage: %s <IP> <PORT>\n", in);
-    printf("Usage: Will require 'sudo' to run if the executable is not setuid root]n");
-    exit(1);
+void *recMsg(void *socket) {
+    int ret;
+    int sockfd = ((int) socket);
+    char buffer[BUF_SIZE];
+    //memset(buffer, 0, BUF_SIZE);
+
+    for(;;) {
+        ret = recvfrom(sockfd, buffer, BUF_SIZE, 0, NULL, NULL);
+
+        if(ret < 0) {
+            printf("Error receiving data.\n");
+        } else {
+            printf("client: %s\n", buffer);
+            //printf("client: ");
+            //fputs(buffer, stdout);
+            //printf("\n");
+        }
+    }
+}
+
+void sig_handler(int signo) {
+    if(signo == SIGINT) {
+        // Kill the client program somehow here
+        close(gnewsockfd);
+        close(gsockfd);
+        printf("Caught sigint\n"); 
+        exit(1);
+    }
 }
