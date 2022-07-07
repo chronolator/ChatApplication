50351fc151cff3534f78dea844ce39f43568a92b
chronolator <mikestuzi@gmail.com>
2022-07-07 12:31:30 -0500

    modified:   client.c

--- client.c
+++ client.c
@@ -8,8 +8,12 @@
 #include <netinet/in.h>
 #include <netdb.h>
 #include <signal.h>
+#include <ncurses.h>
 
 #define BUF_SIZE 256
+
+int sockfd, ret;
+struct sockaddr_in ser_addr, cli_addr;
 
 void *recMsg(void *socket);
 void sig_handler(int signo);
@@ -17,15 +21,12 @@ void sig_handler(int signo);
 int main(int argc, char *argv[]) {
     const int PORT = 8000;
 
-    int ret;
-    int sockfd = (int) socket;
     char buffer[BUF_SIZE];
     char *server_address;
     pthread_t rThread;
-    struct sockaddr_in ser_addr, cli_addr;
-        ser_addr.sin_family = AF_INET;
-        ser_addr.sin_port = htons(PORT);
-        ser_addr.sin_addr.s_addr = INADDR_ANY;
+    ser_addr.sin_family = AF_INET;
+    ser_addr.sin_port = htons(PORT);
+    ser_addr.sin_addr.s_addr = INADDR_ANY;
 
     // Args
     if(argc < 2) {
@@ -49,7 +50,7 @@ int main(int argc, char *argv[]) {
     printf("Socket created\n");
 
     // Conenct to socket
-    ret = connect(sockfd, (struct sockaddr_in *) &ser_addr, sizeof(ser_addr));
+    ret = connect(sockfd, (struct sockaddr *) &ser_addr, sizeof(ser_addr));
     if(ret < 0) {
         printf("Error connecting to the server\n");
         exit(1);
@@ -57,7 +58,6 @@ int main(int argc, char *argv[]) {
     printf("Connected to the server\n");
 
     // Create pthread for receiving messages from server
-    printf("CLIENT> ");
     ret = pthread_create(&rThread, NULL, recMsg, (void *) sockfd);
     if(ret < 0) {
         printf("Error from pthread_create() is %d\n", ret);
@@ -65,10 +65,13 @@ int main(int argc, char *argv[]) {
     }
 
     // Stdin data sent to the server
+    printf("CLIENT> ");
     while(fgets(buffer, BUF_SIZE, stdin) != NULL) {
+        printf("CLIENT> ");
         ret = sendto(sockfd, buffer, BUF_SIZE, 0, (struct sockaddr *) &ser_addr, sizeof(ser_addr));
         if(ret < 0) {
             printf("Error sending the data below:\n\t%s\n", buffer);
+            exit(1);
         }
     }
     
@@ -78,18 +81,25 @@ int main(int argc, char *argv[]) {
     return EXIT_SUCCESS;
 }
 
+// Pthread receiving a message from the server socket
 void *recMsg(void *socket) {
-    int ret;
-    int sockfd = (int) socket;
-    char buffer[BUF_SIZE];
+    int rret;
+    int rsockfd = ((int) socket);
+    char rbuffer[BUF_SIZE];
 
     for(;;) {
-        ret = recvfrom(sockfd, buffer, BUF_SIZE, 0, NULL, NULL);
+        rret = recvfrom(rsockfd, rbuffer, BUF_SIZE, 0, NULL, NULL);
 
-        if(ret < 0) {
+        if(rret < 0) {
             printf("Error receiving data\n");   
         } else {
-            printf("server: %s\n", buffer);
+            if(strncmp(rbuffer, "/exit\0", BUF_SIZE) == 0) {
+                printf("Obtained the exit\n");
+                close(sockfd);
+                //pthread_exit(NULL);
+                exit(1);
+            }
+            printf("\nserver: %s", rbuffer);
             //printf("server: "),;
             //fputs(buffer, stdout);
             //printf("\n");
@@ -97,9 +107,18 @@ void *recMsg(void *socket) {
     }
 }
 
+// SIGINT will terminate both sockets
 void sig_handler(int signo) {
     if(signo == SIGINT) {
+        char rbuffer[BUF_SIZE] = "/exit\0";
+        ret = sendto(sockfd, rbuffer, BUF_SIZE, 0, (struct sockaddr *) &ser_addr, sizeof(ser_addr));
+        if(ret < 0) {
+            printf("Error sending the data below:\n\t%s\n", rbuffer);
+            exit(1);
+        }
 
+        close(sockfd);
+        //pthread_exit(NULL);
         printf("Caught sigint\n");
         exit(1);
     }
